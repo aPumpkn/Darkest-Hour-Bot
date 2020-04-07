72f218ce829967f15814596e666df9dc7829ee5e
aPumpkn <49935147+aPumpkn@users.noreply.github.com>
2020-04-07 18:04:46 -0500

    Added some more methods
    more utils, should be self-explanatory.

--- src/pumpkin/bot/darkest_hour/core/utils/Tool.java
+++ src/pumpkin/bot/darkest_hour/core/utils/Tool.java
@@ -1,8 +1,19 @@
-package pumpkin.bot.darkest_hour.core.utils;
+package pumpkin.darkest_dawn.core.utils;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 // List of useful tools the bot uses for menial tasks.
 public class Tool {
 
+	
+	private int integer;
+	NumberFormat numeric = NumberFormat.getNumberInstance(Locale.US);
+	
+	
 	/* Returns a new, properly cased String. Uses split()
 	 * to replace dashes and underscores with spaces in
 	 * order to determine when to make a new capitalized
@@ -13,12 +24,72 @@ public class Tool {
 	 */
 	public String capitalize(String content) {
 		
-		String[] array = content.split("-_");
+		String[] array = content.split("[-_]");
 		content = "";
 		
 		for (String a : array) content += a.substring(0,1).toUpperCase() + a.substring(1).toLowerCase() + " ";
 		return content.trim();
 		
 	}
+	
+	/* Divides the given List of Strings into a List
+	 * in order to send messages without breaking the
+	 * 2,000 character limit on Discord.
+	 */
+	public List<String> sect(String content) {
+		
+		List<String> list = new ArrayList<String>(0);
+		Scanner scanner = new Scanner(content);
+		int counter = 0;
+		String part = "";
+		
+		while (scanner.hasNextLine()) {
+			
+			if (counter > 49) {
+				
+				list.add(part);
+				part = "";
+				counter = 0;
+				
+			} else {
+				
+				part += scanner.nextLine();
+				counter++;
+			
+			}
+			
+		}
 
+		scanner.close();
+		if (!part.isEmpty()) list.add(part);
+		System.out.println(list.size());
+		return list;
+		
+	}
+
+	/* Attempts to parse an Integer from the given
+	 * string. If the passed String does not contain
+	 * a character that can be parsed into an Integer,
+	 * it returns false. Otherwise, it returns true.
+	 */
+	public boolean isInteger(String content) {
+		
+		try { integer = Integer.parseInt(content); }
+		catch (NumberFormatException e) { return false; }
+		return true;
+		
+	}
+	
+	public String format(int number) {
+		
+		return numeric.format(number);
+		
+	}
+	
+	public int getInteger() {
+		
+		return integer;
+		
+	}
+	
 }
