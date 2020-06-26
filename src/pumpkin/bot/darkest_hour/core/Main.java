3084233d62eaf883814b00de39bb43e69281bbe3
aPumpkn <49935147+aPumpkn@users.noreply.github.com>
2020-06-26 00:18:38 -0500

    Update Main.java

--- src/pumpkin/bot/darkest_hour/core/Main.java
+++ src/pumpkin/bot/darkest_hour/core/Main.java
@@ -18,12 +18,13 @@ public class Main {
 		
 		JDA api = null;
 		boolean quit = false;
+		Listener listener = new Listener();
 		
 		try {
 			
 			System.out.println("\r\n____________________________________________________\r\n\r\n[Darkest Dawn] API Initializing...\r\n");
-			api = new JDABuilder(AccountType.BOT).setToken("lol no").build();
-			api.addEventListener(new Listener());
+			api = new JDABuilder(AccountType.BOT).setToken("NTM0ODgzNjAzNTA3NzA3OTA0.Xo0MZw.gtC3Y42vItA5DfnqPeUn-iR879g").build();
+			api.addEventListener(listener);
 			api.awaitReady();
 			
 		} catch (LoginException | InterruptedException e) {
@@ -44,13 +45,20 @@ public class Main {
 		while (!quit) {
 			
 			System.out.print("> ");
+			String response = scanner.nextLine();
+			String[] command = response.trim().toLowerCase().replaceAll(" +", " ").split(" ");
 			
-			switch (scanner.nextLine().trim().toLowerCase()) {
+			switch (command[0]) {
 				
 				case "quit":
 					System.out.println("\r\n[Darkest Dawn] Closing current connection...");
 					api.shutdown();
 					quit = true;
+					break;
+					
+				case "say":
+					if (command.length < 2) System.out.println("\r\nTo safely close the program, type 'quit'.\r\n");
+					else listener.sendGuildMessage(response.substring(3));
 					break;
 				
 				default:
